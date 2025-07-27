e4f20dc37494eb891c610aa06e5179f74ea1c93c
stefan88scholz <stscholz@mailbox.org>
2025-07-27 22:41:43 +0200

    add argument parser

--- python/main.py
+++ python/main.py
@@ -1,32 +1,28 @@
 import os
+import argparse
 
-root_path = r'C:\Users\stsch\Documents\Feuerwehr'
-if __name__ == '__main__':
-    #tuple_subdir = os.walk(root_path)
-    #print(type(tuple_subdir))
-    #for subdir in tuple_subdir:
-        #print(type(subdir))
-        #print(subdir)
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        prog='change-name-file-dir'
+    )
+    parser.add_argument('path', help='Enter the path of directory or file')
+    args = parser.parse_args()
+    print(args.path)
     # Get list of subdirectories and files of root_path (not recursive)
-    list_dir = os.listdir(root_path)
-    print(os.path.isdir(root_path))
+    list_dir = os.listdir(args.path)
     for name in list_dir:
-        #first create full path name, e.g. C:\\Users\\stsch\\Documents\\Feuerwehr\10 Sichern gegen Absturz
-        full_name = os.path.join(root_path,name)
+        #first create full path name
+        full_name = os.path.join(args.path,name)
+        print(full_name)
         #check if entry is dir
         if os.path.isdir(full_name):
             #replace whitespace with underscore
             new_name = full_name.replace(' ','_')
-            os.rename(full_name,new_name)
+            #os.rename(full_name,new_name)
         #print(name + ' | ' + os.path.isdir(os.path.realpath(name)).__str__())
         #name_new = name.replace(' ', '_')
         #src = os.path.join(root_path)
         #os.rename()
 
-
-    #path = input('Enter the path of your files:')
-    #print(path)
-    #files = os.listdir(path)
-    #for file in files:
-    #    file.
-    #    print(file)
+if __name__ == '__main__':
+    main()
